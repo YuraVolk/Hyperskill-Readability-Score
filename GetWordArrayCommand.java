ce0dd9bd2c1a3a0024a475c5b116dba442ebdbf4
YuraVolk <42916748+YuraVolk@users.noreply.github.com>
2019-11-11 16:40:03 +0300

    Create GetWordArrayCommand.java

---
+++ GetWordArrayCommand.java
@@ -0,0 +1,26 @@
+package readability;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class GetWordArrayCommand extends Command {
+    GetWordArrayCommand(Score score) {
+        super(score);
+    }
+
+    @Override
+    void execute() {
+        List<String> words = new ArrayList<>();
+
+        Pattern pattern = Pattern.compile("[a-zA-Z]+");
+        Matcher matcher = pattern.matcher(score.text);
+
+        while (matcher.find()) {
+            words.add(matcher.group());
+        }
+
+        score.words = words;
+    }
+}
