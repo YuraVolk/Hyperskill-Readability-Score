3146acf85f1c7161bb09fa3fa476fd0037464d22
YuraVolk <42916748+YuraVolk@users.noreply.github.com>
2019-11-11 16:39:43 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+package readability;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        new Score(scanner.nextLine()).getScore();
+    }
+}
